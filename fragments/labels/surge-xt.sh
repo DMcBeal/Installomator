35249aeb2466f515a413f19cbdc908bbc3aabed5
Nani <173367773+DMcBeal@users.noreply.github.com>
2025-01-27 09:00:41 +0000

    Create surge-xt.sh
    Label for Surge XT.

---
+++ fragments/labels/surge-xt.sh
@@ -0,0 +1,8 @@
+surge-xt)
+    name="Surge XT"
+    type="pkgInDmg"
+    downloadURL=$(downloadURLFromGit surge-synthesizer releases-xt)
+    appNewVersion=$(versionFromGit surge-synthesizer releases-xt)
+    packageID=
+    expectedTeamID="547U7G7MZC"
+    ;;
