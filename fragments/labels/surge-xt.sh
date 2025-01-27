fd9a18732bf1fb9aaee81cca71a59f0dc3f03d0e
Nani <173367773+DMcBeal@users.noreply.github.com>
2025-01-27 09:17:54 +0000

    Update surge-xt.sh

--- fragments/labels/surge-xt.sh
+++ fragments/labels/surge-xt.sh
@@ -3,6 +3,5 @@ surge-xt)
     type="pkgInDmg"
     downloadURL=$(downloadURLFromGit surge-synthesizer releases-xt)
     appNewVersion=$(versionFromGit surge-synthesizer releases-xt)
-    packageID=
     expectedTeamID="547U7G7MZC"
     ;;
